9f90dc1b3d6b596569f4a2ef3c8e8abd6a90ca0a
kaede <kaede3256@gmail.com>
2023-04-14 17:38:07 +0900

    画面はできました

--- src/pages/results.js
+++ src/pages/results.js
@@ -1,8 +1,8 @@
-// ResultsPage.js
 import Header from "@/components/Header";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import MapContainer from "@/components/MapContainer";
+import { Flex, Button, Box, Text } from "@chakra-ui/react";
 
 const ResultsPage = () => {
   const router = useRouter();
@@ -19,7 +19,9 @@ const ResultsPage = () => {
   useEffect(() => {
     const fetchResults = async () => {
       if (lat && lng) {
-        const response = await fetch(`/api/search?lat=${lat}&lng=${lng}&radius=1500&type=gym,sauna`);
+        const response = await fetch(
+          `/api/search?lat=${lat}&lng=${lng}&radius=1500&type=gym,sauna`
+        );
         const data = await response.json();
         setPlaces(data);
       }
@@ -30,10 +32,40 @@ const ResultsPage = () => {
   return (
     <>
       <Header />
-      <div style={{ height: "100vh", width: "100%" }}>
-        <h1>検索結果</h1>
+      <Box style={{ height: "100vh", width: "100%" }}>
+        <Text
+          fontFamily="游ゴシック, YuGothic, sans-serif"
+          fontSize={"25px"}
+          fontWeight={"bold"}
+          textAlign={"center"}>
+          検索結果
+        </Text>
         {location && <MapContainer location={location} places={places} />}
-      </div>
+      </Box>
+
+      <Flex
+        justifyContent="center"
+        alignItems="center"
+        bg="#F7FAFC"
+        padding="20px"
+      >
+        <Button
+          onClick={() => router.push("/")}
+          fontWeight="bold"
+          borderRadius="50px"
+          px={20}
+          py={10}
+          bg="#38B6FF"
+          color="white"
+          mt="40px"
+          fontFamily="游ゴシック, YuGothic, sans-serif"
+          cursor="pointer"
+          _hover={{ cursor: "pointer" }}
+          width="200px"
+        >
+          戻る
+        </Button>
+      </Flex>
     </>
   );
 };
