f5b2b100cc71f86d61fcb3cb5f8102e0dff27b54
kaede <kaede3256@gmail.com>
2023-04-14 14:57:45 +0900

    検索結果出るように書いてつもりですがうまく行かないので、コードレビューお願いいsます

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
+import { Button } from "@chakra-ui/react";
 
 const containerStyle = {
   width: '100%',
@@ -14,6 +15,7 @@ const center = {
 
 const MapContainer = ({ places }) => {
   const [location, setLocation] = useState(null);
+  const [map, setMap] = useState(null);
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -23,19 +25,51 @@ const MapContainer = ({ places }) => {
     }
   }, []);
 
+  const handleCenterChange = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLocation(position.coords);
+        setMap((prevMap) => {
+          const map = prevMap;
+          map.setCenter({ lat: position.coords.latitude, lng: position.coords.longitude });
+          return map;
+        });
+      });
+    }
+  };
+
+  const onLoad = useCallback((map) => {
+    setMap(map);
+  }, []);
+
   return (
-    <LoadScript googleMapsApiKey="AIzaSyBrTOdd5nn6gYQ5UL9ODhRDjnhZlOyuVIA">
+    <LoadScript
+      googleMapsApiKey="AIzaSyBrTOdd5nn6gYQ5UL9ODhRDjnhZlOyuVIA"
+    >
       <GoogleMap
+        onLoad={onLoad}
         mapContainerStyle={containerStyle}
         center={location ? { lat: location.latitude, lng: location.longitude } : center}
         zoom={13}
-      />
+      >
+        {places && places.map((place) => (
+          <Marker key={place.id} position={{ lat: place.latitude, lng: place.longitude }} />
+        ))}
 
-      {places && places.map((place) => (
-        <Marker key={place.id} position={{ lat: place.latitude, lng: place.longitude }} />
-      ))}
-
-
+        <div style={{ position: "absolute", top: "10px", right: "10px" }}>
+          <Button onClick={handleCenterChange} fontWeight="bold"
+          borderRadius="50px"
+          px={20}
+          py={10}
+          bg="#38B6FF"
+          color="white"
+          fontFamily="游ゴシック, YuGothic, sans-serif"
+          cursor="pointer"
+          _hover={{ cursor: "pointer" }}>
+            現在地に戻る
+          </Button>
+        </div>
+      </GoogleMap>
     </LoadScript>
   );
 };
