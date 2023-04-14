933f260cc732783b283fdb037c79b30064070991
kaede <kaede3256@gmail.com>
2023-04-14 19:04:46 +0900

    firebaseやってみましたが、うまくできません

--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -1,22 +1,15 @@
-// pages/api/search.js
-
 import { getNearbyPlaces } from "../../../lib/google";
-import firebase from "../../../lib/firebase";
+import { addPlace } from "../../../lib/firebase";
 
 export default async function handler(req, res) {
   const { lat, lng, radius, type } = req.query;
   const response = await getNearbyPlaces(lat, lng, radius, type);
   const data = await response.json();
-  
-  const db = firebase.firestore();
-  const collectionRef = db.collection("places");
-  
+
   // Firebase にデータを登録する
   data.results.forEach(async (result) => {
-    const docRef = collectionRef.doc(result.place_id);
-    await docRef.set(result);
+    await addPlace(result);
   });
-  
+
   res.status(200).json(data.results);
 }
-
